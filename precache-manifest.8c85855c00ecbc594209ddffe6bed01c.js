49fe72e8a7e682db2cfc2f87e18565730d083b4a
ikehcharles <ikehcharlesci@gmail.com>
2020-06-29 16:38:06 +0100

    Updates

---
+++ precache-manifest.8c85855c00ecbc594209ddffe6bed01c.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad13ee2be26cfbe970905fe94fe375e5",
+    "url": "/firstReact/index.html"
+  },
+  {
+    "revision": "7e9e90a8bcf4b97342c3",
+    "url": "/firstReact/static/css/main.872e0f13.chunk.css"
+  },
+  {
+    "revision": "3c21177e4c047403eded",
+    "url": "/firstReact/static/js/2.17db302a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/firstReact/static/js/2.17db302a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9e90a8bcf4b97342c3",
+    "url": "/firstReact/static/js/main.aeac05a6.chunk.js"
+  },
+  {
+    "revision": "4653e7a47c79822f0148",
+    "url": "/firstReact/static/js/runtime-main.c4deb5e8.js"
+  },
+  {
+    "revision": "0c76d52b1c04cb922ac745d980a27787",
+    "url": "/firstReact/static/media/about0.0c76d52b.jpg"
+  },
+  {
+    "revision": "6f12dc822323bf4eb146fa4409948694",
+    "url": "/firstReact/static/media/about1.6f12dc82.jpg"
+  },
+  {
+    "revision": "bdc53ab62d6cacadabaf9ab0b4793f15",
+    "url": "/firstReact/static/media/about2.bdc53ab6.jpg"
+  },
+  {
+    "revision": "b7b0685658aade276df34fcbe1497fba",
+    "url": "/firstReact/static/media/about3.b7b06856.jpg"
+  },
+  {
+    "revision": "c1c5c41f16e3d27a3cc515296c765317",
+    "url": "/firstReact/static/media/about4.c1c5c41f.jpg"
+  },
+  {
+    "revision": "4654353bdddc514e1b8588bcfe1d6c0a",
+    "url": "/firstReact/static/media/about5.4654353b.jpg"
+  },
+  {
+    "revision": "b8a6c7f7c88c83120eaf8178d9d8fe3c",
+    "url": "/firstReact/static/media/about6.b8a6c7f7.jpg"
+  },
+  {
+    "revision": "ed52259ee7aa8f9ad73052947ecf372e",
+    "url": "/firstReact/static/media/about7.ed52259e.jpg"
+  },
+  {
+    "revision": "024fa0ed301d2618c8dace08eeeba57a",
+    "url": "/firstReact/static/media/about8.024fa0ed.jpg"
+  },
+  {
+    "revision": "6b6cf25685f61d89e766771deb0ecdd9",
+    "url": "/firstReact/static/media/about9.6b6cf256.jpg"
+  },
+  {
+    "revision": "fbf68a5df3c5ce5437b5f67fafb05b7c",
+    "url": "/firstReact/static/media/aboutHeader.fbf68a5d.jpg"
+  },
+  {
+    "revision": "9fb1e07735e584b2f1cbef316c292376",
+    "url": "/firstReact/static/media/foodScroll1.9fb1e077.jpg"
+  },
+  {
+    "revision": "cda59b2430452602060f29d5bdccb264",
+    "url": "/firstReact/static/media/foodScroll2.cda59b24.jpg"
+  },
+  {
+    "revision": "cf471826137507753be30353d1985384",
+    "url": "/firstReact/static/media/foodScroll3.cf471826.jpg"
+  },
+  {
+    "revision": "666e20152915a08e3d7073589ef0e323",
+    "url": "/firstReact/static/media/foodScroll4.666e2015.jpg"
+  },
+  {
+    "revision": "b1ca19a9003e333a8c4f3b8d044e7467",
+    "url": "/firstReact/static/media/foodScroll5.b1ca19a9.jpg"
+  },
+  {
+    "revision": "0e172be34e3fd93f07a4133ced38175b",
+    "url": "/firstReact/static/media/mcCafe1.0e172be3.jpg"
+  },
+  {
+    "revision": "c0a71fc3eb594df0f198562834aa75bc",
+    "url": "/firstReact/static/media/mcCafe10.c0a71fc3.jpg"
+  },
+  {
+    "revision": "955b1651ae94b6fb4b5262038283db31",
+    "url": "/firstReact/static/media/mcCafe2.955b1651.jpg"
+  },
+  {
+    "revision": "69e9cf3b00c35b812a1b90ad39fd27f3",
+    "url": "/firstReact/static/media/mcCafe3.69e9cf3b.jpg"
+  },
+  {
+    "revision": "f07c7447f53b106500b64f9761405779",
+    "url": "/firstReact/static/media/mcCafe4.f07c7447.jpg"
+  },
+  {
+    "revision": "8de4950bec7b79e73a49e04940f9c720",
+    "url": "/firstReact/static/media/mcCafe5.8de4950b.jpg"
+  },
+  {
+    "revision": "fd23f67cc0ed7dcd03147ccf16a105ac",
+    "url": "/firstReact/static/media/mcCafe6.fd23f67c.jpg"
+  },
+  {
+    "revision": "7576f66b01be83841d195e5776417cfd",
+    "url": "/firstReact/static/media/mcCafe7.7576f66b.jpg"
+  },
+  {
+    "revision": "2cc0d6f973c190f51e43a978e6ae1f8a",
+    "url": "/firstReact/static/media/mcCafe8.2cc0d6f9.jpg"
+  },
+  {
+    "revision": "80e878df3cc2fe45ece7ce78c255ddb1",
+    "url": "/firstReact/static/media/mcCafe9.80e878df.jpg"
+  },
+  {
+    "revision": "ca6a2889eb31b7504b5b72c34fe3fa4d",
+    "url": "/firstReact/static/media/mccafe_hub_hero.ca6a2889.jpg"
+  },
+  {
+    "revision": "cd76f89ade79bbcd56b7a92c4955c58c",
+    "url": "/firstReact/static/media/meal.cd76f89a.png"
+  },
+  {
+    "revision": "afc1ff1294fa636edba83a88a23d306b",
+    "url": "/firstReact/static/media/meal2.afc1ff12.jpg"
+  },
+  {
+    "revision": "842b4bd8a8b34250bf7c769fabd0b380",
+    "url": "/firstReact/static/media/meal3.842b4bd8.jpg"
+  },
+  {
+    "revision": "28f90ede0d26cc840c5c7c4de21764b2",
+    "url": "/firstReact/static/media/trending1.28f90ede.jpg"
+  },
+  {
+    "revision": "6b32374aca7a537559e9a56bff3a82e3",
+    "url": "/firstReact/static/media/trending2.6b32374a.jpg"
+  },
+  {
+    "revision": "53161de41ac332d5e41224974b6680c5",
+    "url": "/firstReact/static/media/trending3.53161de4.jpg"
+  }
+]);
